364c5907117f57b4674cbb3a16ab32293c7d89d2
Eike Merx <eike.merx@sapiens.com>
2023-07-23 15:42:00 +0200

    feat: fix routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,13 +22,13 @@ function Header({ pokemons }) {
                 alt="Pokemon fight logo"
               />
           </Navbar.Brand>
-          <Nav className="ml-auto">
+          {/* <Nav className="ml-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/pokemons">Pokedex</Nav.Link>
             <Nav.Link href="/arena">
               <i className="fa-solid fa-swords"></i> Arena
             </Nav.Link>
-          </Nav>
+          </Nav> */}
         </Container>
       </Navbar>
     </>
